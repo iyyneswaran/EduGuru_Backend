565e275516daa51db5eb461bad079092c2f2c372
iyyneswaran <pdee5660@gmail.com>
2026-03-08 13:44:18 +0530

    fix(api): allow localhost:3000 in cors config

--- api/src/app.ts
+++ api/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://edu-guru-frontend.vercel.app"],
+        origin: ["http://localhost:5173", "http://localhost:3000", "https://edu-guru-frontend.vercel.app"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
